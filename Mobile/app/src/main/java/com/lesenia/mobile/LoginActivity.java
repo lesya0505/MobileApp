f3e9b6817bbf64d80bbca2b0411788cfe0ae6b2d
lesenia05 <33019458+lesenia05@users.noreply.github.com>
2019-10-26 23:40:34 +0300

    lab_1

---
+++ Mobile/app/src/main/java/com/lesenia/mobile/LoginActivity.java
@@ -0,0 +1,115 @@
+package com.lesenia.mobile;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private Button loginButton;
+    private TextView signUpLink;
+    private TextInputEditText emailField;
+    private TextInputEditText passwordField;
+    private TextInputLayout emailFieldLayout;
+    private TextInputLayout passwordFieldLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fieldsInit();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = Objects.requireNonNull(Objects.requireNonNull(emailField.getText()).toString().trim());
+                final String password = Objects.requireNonNull(passwordField.getText()).toString().trim();
+                logIn(email, password);
+            }
+        });
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void fieldsInit() {
+        auth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.login_email);
+        passwordField = findViewById(R.id.login_password);
+        emailFieldLayout = findViewById(R.id.login_layout_email);
+        passwordFieldLayout = findViewById(R.id.login_layout_password);
+        loginButton = findViewById(R.id.login_loginButton);
+        signUpLink = findViewById(R.id.login_signUpLink);
+    }
+
+    private void logIn(final String email, final String password) {
+
+        if (isDataValid(email, password)) {
+            Task<AuthResult> authResultTask = auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, UserActivity.class));
+                            } else {
+                                loginError();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean isEmailValid(final String email) {
+        if (email.isEmpty()) {
+            emailFieldLayout.setError(getString(R.string.enter_email));
+            return false;
+        } else {
+            emailFieldLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean isPasswordValid(final String password) {
+        if (password.isEmpty() || password.length() < 8) {
+            passwordFieldLayout.setError(getString(R.string.invalid_password));
+            return false;
+        } else {
+            passwordFieldLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean isDataValid(final String email, final String password) {
+        boolean emailValid = isEmailValid(email);
+        boolean passwordValid = isPasswordValid(password);
+
+        return emailValid && passwordValid;
+    }
+
+    private void loginError() {
+        Toast.makeText(LoginActivity.this, getString(R.string.login_error), Toast.LENGTH_LONG).show();
+    }
+}
